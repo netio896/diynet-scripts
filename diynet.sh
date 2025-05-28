d981ed0e5cdd0653df2172bee985f5b3001953c4
netio896 <nainghtweyan419@gmail.com>
2025-05-28 16:37:05 +0630

    添加 HTML 命令生成器 gen-html-snih

--- diynet.sh
+++ diynet.sh
@@ -14,13 +14,13 @@ case $category in
     echo "1) 安装常用工具"
     echo "2) 网络优化"
     read -p "选择脚本编号：" tool
-    [[ $tool == 1 ]] && bash <(curl -sSL https://raw.githubusercontent.com/netio896/diynet-scripts/main/scripts/base-tools.sh)
-    [[ $tool == 2 ]] && bash <(curl -sSL https://raw.githubusercontent.com/netio896/diynet-scripts/main/scripts/net-optimize.sh)
+    [[ $tool == 1 ]] && bash <(curl -sSL https://gitspeed.diynet.club/https://raw.githubusercontent.com/netio896/diynet-scripts/main/scripts/base-tools.sh)
+    [[ $tool == 2 ]] && bash <(curl -sSL https://gitspeed.diynet.club/https://raw.githubusercontent.com/netio896/diynet-scripts/main/scripts/net-optimize.sh)
     ;;
   2)
     echo "1) 创建 Pulse 容器"
     read -p "选择脚本编号：" tool
-    [[ $tool == 1 ]] && bash <(curl -sSL https://raw.githubusercontent.com/netio896/diynet-scripts/main/pve/create-lxc-pulse.sh)
+    [[ $tool == 1 ]] && bash <(curl -sSL https://gitspeed.diynet.club/https://raw.githubusercontent.com/netio896/diynet-scripts/main/pve/create-lxc-pulse.sh)
     ;;
   3)
     echo "1) 部署 Pulse Server 到群晖"
